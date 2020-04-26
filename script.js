379845485db98a8b81c9c498a871efea997ea821
Keven Molina <kevenmolina@gmail.com>
2020-04-25 20:16:56 -0500

    added index,style and script

---
+++ script.js
@@ -0,0 +1,25 @@
+// *************************
+console.log("Hello");
+const jobNumber = document.querySelector('#jobCnt');
+const hoursNumber = document.querySelector('#hours');
+const submitBtn = document.querySelector('#submitBtn')  
+//**********Functions******************
+
+function perTime(){
+    let perHour = jobNumber.value / hoursNumber.value
+    let subHours = []
+    for(let i = 0; i < 2; i++)
+    //sub output
+    //sub-sub output
+    
+    //store in "other results"
+
+}
+//****************************
+
+//****************************
+//****
+
+//************************ task pacer(distributed beeps indicating stages)
+//how many stages
+//how much time do you have to work with (Maybe it could be add up towards the job count)
